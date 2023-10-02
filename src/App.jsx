c56b3dc5ccabff826d14c23de17e5bc62c9d6467
rami jawadi <ramijawadi104@gmail.com>
2023-10-02 15:32:50 +0100

    add Delete function

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,31 @@ setTodos(currentTodos => {
 setNewItem('')
 }
 
+
+const toggleTodo = (id , completed) => {
+
+  setTodos(currentTodos => {
+
+    return currentTodos.map(todo => {
+
+      if(todo.id === id ) {
+
+        return {...todo , completed}
+      }
+return todo
+    })
+  })
+}
+
+
+const deleteTodo = (id) => {
+setTodos(currentTodos => {
+
+  return currentTodos.filter(todo =>todo.id !== id)
+})
+
+}
+
   return (
     <>
       <form onSubmit={handleSubmit} className="new-item-form">
@@ -29,16 +54,18 @@ setNewItem('')
       <h1 className="header">Todos List</h1>
 
       <ul className="list">
+
+        {todos.length === 0 && "No Todos ..."}
 {todos.map(todo  => {
 
 return (
  
 <li key={todo.id}>
 <label>
-  <input type='checkbox' checked={todo.completed} />
+  <input type='checkbox' checked={todo.completed}  onChange={e=> toggleTodo(todo.id , e.target.checked)} />
   {todo.title}
 </label>
-<button className="btn btn-danger">Delete</button>
+<button onClick={()=> deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
 </li>
 
 )
